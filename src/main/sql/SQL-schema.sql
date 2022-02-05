e90243ca66a6258a74b6ec9e4a4373b910823447
Anatoliy Grigorev <toyvokalevala@gmail.com>
2022-02-05 11:15:08 +0300

    Make minor changes in SQL schema after review

--- src/main/sql/SQL-schema.sql
+++ src/main/sql/SQL-schema.sql
@@ -4,13 +4,13 @@ use audiotracks;
 
 create table users(
 id int primary key auto_increment,
-firstname varchar(50) not null,
-lastname varchar(50) not null,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
 login varchar(50) not null,
 password varchar(32) not null,
 is_admin tinyint not null,
-useraccount decimal(5,2) not null default 0,
-userdiscount int not null default 0
+account decimal(5,2) not null default 0,
+discount int not null default 0
 );
 
 create table albums(
@@ -27,10 +27,8 @@ create table tracks(
 id int primary key auto_increment,
 name varchar(50) not null,
 album_id int not null,
-assembly_id int not null,
 price decimal(5,2) not null, 
-Foreign Key(album_id) references albums(id),
-Foreign Key(assembly_id) references assemblies(id)
+Foreign Key(album_id) references albums(id)
 );
  
 create table orders(
@@ -38,6 +36,25 @@ id int primary key auto_increment,
 user_id int not null,
 track_id int not null,
 is_paid tinyint not null default 0,
+order_date datetime,
 Foreign Key(user_id) references users(id),
 Foreign Key(track_id) references tracks(id)
+);
+
+create table feedbacks(
+id int primary key auto_increment,
+user_id int not null,
+track_id int not null,
+feedback_date datetime,
+feedback_text varchar(500),
+Foreign Key(user_id) references users(id),
+Foreign Key(track_id) references tracks(id)
+);
+
+create table tracks_assemblies(
+id int primary key auto_increment,
+track_id int not null,
+assembly_id int not null,
+Foreign Key(track_id) references tracks(id),
+Foreign Key(assembly_id) references assemblies(id)
 );
